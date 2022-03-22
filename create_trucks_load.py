78b9d545535af0df8a2fde2679d8f0f0dce368e1
mircoceccarelli <mirco.ceccarelli@stud.unifi.it>
2022-03-22 11:04:23 +0100

    added daily load stats

--- create_trucks_load.py
+++ create_trucks_load.py
@@ -1,9 +1,34 @@
 import pandas as pd
+import csv
+from statistics import mean
 
 trucks_load = pd.read_csv("trucks_paths.csv")
+text = trucks_load['date'][0][:-14]
 
 trucks_load['tot_weight'] = trucks_load.groupby(['truck_num'])['weight'].transform('sum')
 trucks_load = trucks_load.drop_duplicates(subset=['truck_num'], keep='last')
 trucks_load = trucks_load[['truck_num', 'tot_weight']]
 
 trucks_load.to_csv("trucks_load.csv", encoding='utf-8', index=False)
+
+with open("trucks_load.csv", 'r') as myfile:
+    reader = csv.reader(myfile)
+    skip = next(reader, None)
+    trucks_path_weights = []
+    for row in reader:
+        trucks_path_weights.append(float(row[1]))
+
+min_weights = min(trucks_path_weights)
+max_weights = max(trucks_path_weights)
+avg_weights = round(mean(trucks_path_weights), 2)
+
+with open('trucks_stats.txt', 'w') as f:
+    f.write("Raccolta giorno: " +str(text))
+    f.write('\n')
+    f.write("· Peso minimo camion a fine giro -> " + str(min_weights) + " Kg")
+    f.write('\n')
+    f.write("· Peso massimo camion a fine giro -> " + str(max_weights) + " Kg")
+    f.write('\n')
+    f.write("· Peso medio camion a fine giro -> " + str(avg_weights) + " Kg")
+
+
